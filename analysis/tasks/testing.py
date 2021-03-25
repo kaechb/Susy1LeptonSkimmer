b974d1d43f46f5ef0c981a2b967e7737a28e29d2
frengelk <frederic.engelke@rwth-aachen.de>
2021-03-25 11:58:23 +0100

    test dummy task for trying things, here htcondor

---
+++ analysis/tasks/testing.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+import logging
+import os
+import law
+import law.contrib.coffea
+from luigi import BoolParameter, Parameter
+from coffea import processor
+from coffea.nanoevents import TreeMakerSchema, BaseSchema, NanoAODSchema
+import json
+import time
+import numpy as np
+from rich.console import Console
+
+import logging
+
+# other modules
+from tasks.basetasks import DatasetTask, HTCondorWorkflow
+from utils.CoffeaBase import *
+from tasks.makefiles import WriteFileset
+
+logger = logging.getLogger(__name__)
+
+
+class TestDummy(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):  # AnalysisTask):
+    def output(self):
+        return self.local_target("fileset.json")
+
+    def run(self):
+        # make the output directory
+
+        print("deeedledoo")
+        self.output().parent.touch()
+
+        # unchanged syntax
+        # test_file = "/nfs/dust/cms/user/frengelk/Testing/TTJets_HT_1200to2500_1.root"
+
+        fileset = {}
+
+        for dat in self.config_inst.datasets:
+            fileset.update(
+                {
+                    dat.name: dat.keys,
+                }
+            )
+
+        print(fileset)
+
+        # from IPython import embed;embed()
+
+        with open(self.output().path, "w") as file:
+            json.dump(fileset, file)
