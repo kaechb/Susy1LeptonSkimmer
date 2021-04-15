f7c3cfd857100b0471af43069809dcee6ac689cf
frengelk <frederic.engelke@rwth-aachen.de>
2021-04-15 16:52:03 +0200

    added data plotting. Can be toggled with unblinded parameter. Produces data points and ration plot in addition to normal plot

--- analysis/tasks/plotting.py
+++ analysis/tasks/plotting.py
@@ -53,11 +53,31 @@ class PlotCoffeaHists(ConfigTask):
         # "font.family": "sans-serif",
         # "font.sans-serif": ["Helvetica"]})
 
+        # declare sytling options
+        error_opts = {
+            "label": "Stat. Unc.",
+            "hatch": "///",
+            "facecolor": "none",
+            "edgecolor": (0, 0, 0, 0.5),
+            "linewidth": 0,
+        }
+        data_err_opts = {
+            "linestyle": "none",
+            "marker": ".",
+            "markersize": 10.0,
+            "color": "k",
+            "elinewidth": 1,
+        }
+        line_opts = {
+            "linestyle": "-",
+            "color": "r",
+        }
+
+
+
         # create pdf object to save figures on separate pages
         with PdfPages(self.output().path) as pdf:
 
-            # print dummy hist, because first one is broken
-            # does not work either FIXME
             # from IPython import embed;embed()
             # plot each hist
             for var in tqdm(self.config_inst.variables, unit="variable"):
@@ -67,21 +87,49 @@ class PlotCoffeaHists(ConfigTask):
 
                 for cat in categories:
 
+                    # lists for collecting and sorting hists
                     bg_hists = []
                     hists_attr = []
+                    data_hists= []
 
-                    # ax = fig.add_subplot(number,1,i+1)
-                    fig = plt.figure()
-                    ax = fig.add_subplot()
+                    # enlargen figure
+                    #plt.figure(figsize=(8, 6), dpi=80)
 
-                    # ax.set_title("{0}: {1}".format(cat, var.name))
-                    ax.autoscale(axis="x", tight=True)
+                    if self.unblinded:
+                        # build canvas for data plotting
+                        fig, (ax, rax) = plt.subplots(
+                                    2,
+                                    1,
+                                    figsize=(18, 10),
+                                    sharex=True,
+                                    gridspec_kw=dict(height_ratios=(3, 1), hspace=0,),
+                                )
+
+
+                    else:
+                        fig, ax = plt.subplots(figsize=(18, 10))
 
                     for proc in self.config_inst.processes:
-                        # from IPython import embed;embed()
-                        # if "QCD" in proc.name:
 
-                        if not self.debug:
+                        if "data" in proc.name and self.unblinded:
+                            child_hists = hists[
+                                    [p[0].name for p in proc.walk_processes()], cat
+                                    ]
+                            mapping = {
+                                proc.label: [p[0].name for p in proc.walk_processes()
+                                             if not "B" in p[0].name]
+                                }
+                            grouped = child_hists.group(
+                                   "dataset",
+                                    coffea.hist.Cat("process", proc.label_short),
+                                    mapping,
+                            ).integrate("category")
+
+                            data_hists.append(grouped)
+
+
+
+                        if not self.debug and not "data" in proc.name:
                             # for each process, map childs together to plot in one, get rid of cat axis
                             child_hists = hists[
                                 [p[0].name for p in proc.walk_processes()], cat
@@ -122,6 +170,7 @@ class PlotCoffeaHists(ConfigTask):
                     bg_order = sorted(order_lut.items(), key=operator.itemgetter(1))
                     order = [name[0][0] for name in bg_order]
 
+                    # plot bg
                     coffea.hist.plot1d(
                         # inp[var.name].integrate("category"),
                         bg,
@@ -142,29 +191,67 @@ class PlotCoffeaHists(ConfigTask):
                         loc=0,
                         ax=ax,
                     )
+                    if self.unblinded:
+                        # normally, we have only two kinds of data, if more, adapt
+                        dat = data_hists[0].add(data_hists[1])
+                        data = dat.group("process",  coffea.hist.Cat("process", "data"), {"data": ['data electron','data muon']})
+                        coffea.hist.plot1d(
+                            data,#.project( "process", varName),
+                            #overlay="process",
+                            error_opts=data_err_opts,
+                            ax=ax,
+                            #overflow="none",
+                            #binwnorm=True,
+                            clear=False,
+                        )
+                        coffea.hist.plotratio(
+                            data.sum("process"),
+                            bg.sum("process"),
+                            ax=rax,
+                            error_opts=data_err_opts,
+                            denom_fill_opts={},
+                            guide_opts={},
+                            unc="num",
+                            clear=False,
+                        )
+                        rax.set_ylabel("Ratio")
+                        rax.set_ylim(0, 2)
+
 
                     # declare naming
                     leg = ax.legend(
                         title="{0}: {1}".format(cat, var.x_title),
                         ncol=1,
-                        loc="upper right",
-                        bbox_to_anchor=(1.04, 1),
+                        loc="upper left",
+                        bbox_to_anchor=(1, 1),
                         borderaxespad=0,
                     )
 
                     if self.log_scale:
                         ax.set_yscale("log")
-                        ax.set_ylim(0.0001, 1e9)
-
-                    # from IPython import embed;embed()
+                        ax.set_ylim(0.0001, 1e12)
+                        # FIXME be careful with logarithmic ratios
+                        #if self.unblinded:
+                        #    rax.set_yscale("log")
+                        #    rax.set_ylim(0.0001, 1e0)
 
                     ax.set_xlabel(var.get_full_x_title())
                     ax.set_ylabel(var.get_full_y_title())
+                    ax.autoscale(axis="x", tight=True)
+
+
+                    if self.unblinded:
+                        rax.set_xlabel(var.get_full_x_title())
+                        rax.set_ylabel("Ratio")
+                        rax.autoscale(axis="x", tight=True)
 
                     plt.tight_layout()
                     pdf.savefig(fig)
-                    ax.cla()
                     fig.clear()
+
+                    ax.cla()
+                    if self.unblinded:
+                        rax.cla()
 
             print("\n", " ---- Created {} pages ----".format(pdf.get_pagecount()), "\n")
 
