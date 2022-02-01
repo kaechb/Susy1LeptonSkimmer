ef30e51f726a25ec0a9142c407003c6637ca6ad6
Benno Kaech <kaechb@users.noreply.github.com>
2022-02-01 17:52:50 +0100

    added setup.sh

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-if [ -z ${conda+x} ];
-then echo "ERROR!!!!! remove all conda entries from Path and conda init from .rc files";
+if  command -v conda &> /dev/null ;
+then
+ echo "ERROR!!!!! remove all conda entries from Path and conda init from .rc files";
 else 
 set -e
+#I would add the following 3 lines to the bashrc
 module load anaconda3/5.0
 source /opt/anaconda3/5.0/etc/profile.d/conda.sh   
 export DUST=/nfs/dust/cms/user/$USER/
+
 cd $DUST
-mkdir Anaconda Anaconda/envs  Anaconda/pkgs
+mkdir -p Anaconda/envs  Anaconda/pkgs
 cd Anaconda
 conda config --add envs_dirs $DUST/Anaconda/envs
 conda config --add pkgs_dirs $DUST/Anaconda/pkgs
@@ -20,7 +23,7 @@ mamba update -n base -c defaults conda -y
 mamba install cudatoolkit=10.2 -c pytorch -y
 mamba install -c conda-forge pytorch -y
 mamba install -c conda-forge law -y
-$DUST/Anaconda/envs/susy1lep/bin/pip install order -y
+$DUST/Anaconda/envs/susy1lep/bin/pip install order
 mamba install -c conda-forge scikit-learn -y
 mamba install -c conda-forge black -y
  fi
