cf3d10a942595c5dc4588bf97debddc3341ddded
frengelk <frederic.engelke@rwth-aachen.de>
2022-03-02 17:11:51 +0100

    Added Task to perform a k-fold split over the dataset and create k different sets of train:validation. Also added new task that performs a training on all k-fold variations, saves performance for all k-folds to json

--- analysis/tasks/arraypreparation.py
+++ analysis/tasks/arraypreparation.py
@@ -9,6 +9,7 @@ import operator
 
 import sklearn.model_selection as skm
 
+
 # other modules
 from tasks.basetasks import ConfigTask
 from tasks.coffea import CoffeaProcessor
@@ -40,12 +41,12 @@ class ArrayNormalisation(ConfigTask):
 
     def output(self):
         # In [9]: for k in self.config_inst.get_aux("DNN_process_template").keys():
-        out = {
-            cat + "_" + proc: self.local_target("normed_" + cat + "_" + proc + ".npy")
-            for proc in self.config_inst.processes.names()
-            for cat in self.config_inst.categories.names()
-            if self.channel in cat and not "data" in proc
-        }
+        out = {}
+        # cat + "_" + proc: self.local_target("normed_" + cat + "_" + proc + ".npy")
+        # for proc in self.config_inst.processes.names()
+        # for cat in self.config_inst.categories.names()
+        # if self.channel in cat and not "data" in proc
+        # }
         out.update(
             {  # "norm_values": self.local_target("norm_values.npy"),
                 "one_hot_labels": self.local_target("one_hot_labels.npy"),
@@ -137,7 +138,7 @@ class ArrayNormalisation(ConfigTask):
 
         # train and validation set 80:20 FIXME
         X_train, X_val, y_train, y_val = skm.train_test_split(
-            X_train, y_train, test_size=0.2, random_state=2
+            X_train, y_train, test_size=0.5, random_state=2
         )
 
         # define means and stds for each variable
@@ -147,3 +148,136 @@ class ArrayNormalisation(ConfigTask):
         # save all arrays away, using the fact that keys have the variable name
         for key in self.output().keys():
             self.output()[key].dump(eval(key))
+
+
+class CrossValidationPrep(ConfigTask):
+
+    kfold = luigi.IntParameter(default=5)
+
+    """
+    Task to modify the Arrays produced by coffea
+    Current idea: normalise them to prepare for the DNN
+    """
+
+    channel = luigi.Parameter(default="N1b_CR", description="channel to prepare")
+
+    def requires(self):
+        return CoffeaProcessor.req(self, processor="ArrayExporter")
+
+    # def output(self):
+    # return self.local_target("hists")
+
+    def output(self):
+        out = {}
+        out.update(
+            {
+                "cross_val_{}".format(i): {
+                    "cross_val_X_train_{}".format(i): self.local_target(
+                        "cross_val_X_train_{}.npy".format(i)
+                    ),
+                    "cross_val_y_train_{}".format(i): self.local_target(
+                        "cross_val_y_train_{}.npy".format(i)
+                    ),
+                    "cross_val_X_val_{}".format(i): self.local_target(
+                        "cross_val_X_val_{}.npy".format(i)
+                    ),
+                    "cross_val_y_val_{}".format(i): self.local_target(
+                        "cross_val_y_val_{}.npy".format(i)
+                    ),
+                }
+                for i in range(self.kfold)
+            }
+        )
+        return out
+
+    def normalise(self, array):
+        return ((array - array.mean()) / array.std(), array.mean(), array.std())
+
+    def calc_norm_parameter(self, data):
+        # return values to shift distribution to normal
+
+        dat = np.swapaxes(data, 0, 1)
+        means, stds = [], []
+        for var in dat:
+            means.append(var.mean())
+            stds.append(var.std())
+
+        return np.array(means), np.array(stds)
+
+    def run(self):
+
+        # load inputs from ArrayExporter
+        target_dict = self.input()["collection"].targets
+
+        # make regular dict out of ordered dict
+        file_dict = {}
+        for i in target_dict.keys():
+            file_dict.update(target_dict[i])
+
+        proc_dict = {}
+        one_hot_labels = []
+
+        # loop through datasets and sort according to aux template
+        datasets = list(file_dict.values())
+
+        # at some point, we have to include k fold cross validation
+        # https://github.com/christianversloot/machine-learning-articles/blob/main/how-to-use-k-fold-cross-validation-with-pytorch.md
+        # from sklearn.model_selection import KFold
+
+        for i, key in enumerate(
+            self.config_inst.get_aux("DNN_process_template").keys()
+        ):
+            proc_list = []
+            for cat in self.config_inst.categories.names():
+                # print(1,cat)
+                for subproc in self.config_inst.get_aux("DNN_process_template")[key]:
+                    # print(2,subproc)
+                    # catch bottom level processes that have no childs
+                    for dat in datasets:
+                        if cat in dat.path and subproc in dat.path:
+                            # self.config_inst.datasets.names():
+                            proc_list.append(dat.load())
+
+                        for subsubproc in self.config_inst.get_process(
+                            subproc
+                        ).walk_processes():
+                            # print(3,dat, cat, subsubproc)
+                            if cat in dat.path and subsubproc[0].name in dat.path:
+                                # load input
+                                # print(4,dat, subsubproc[0].name)
+                                proc_list.append(dat.load())
+
+            # from IPython import embed;embed()
+
+            # print(proc_list)
+            proc_dict.update({key: np.concatenate(proc_list)})
+            # 3 processes
+            labels = np.zeros((len(np.concatenate(proc_list)), 3))
+            labels[:, i] = 1
+            one_hot_labels.append(labels)
+
+        # merge all processes
+        data_compl = np.concatenate(list(proc_dict.values()))
+        one_hot_labels = np.concatenate(one_hot_labels)
+
+        kfold = skm.KFold(n_splits=self.kfold, shuffle=True, random_state=42)
+        # for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
+
+        # kfold returns generator, loop over generated indices
+        # for each kfold, dump the respective data and labels
+        for i, idx in enumerate(kfold.split(data_compl)):
+            # unpack tuple
+            train_idx, val_idx = idx
+
+            self.output()["cross_val_{}".format(i)][
+                "cross_val_X_train_{}".format(i)
+            ].dump(data_compl[train_idx])
+            self.output()["cross_val_{}".format(i)][
+                "cross_val_y_train_{}".format(i)
+            ].dump(one_hot_labels[train_idx])
+            self.output()["cross_val_{}".format(i)][
+                "cross_val_X_val_{}".format(i)
+            ].dump(data_compl[val_idx])
+            self.output()["cross_val_{}".format(i)][
+                "cross_val_y_val_{}".format(i)
+            ].dump(one_hot_labels[val_idx])
--- analysis/tasks/pytorch_test.py
+++ analysis/tasks/pytorch_test.py
@@ -22,7 +22,7 @@ from captum.attr import LayerConductance
 from captum.attr import NeuronConductance
 
 from tasks.basetasks import DNNTask, HTCondorWorkflow
-from tasks.arraypreparation import ArrayNormalisation
+from tasks.arraypreparation import ArrayNormalisation, CrossValidationPrep
 
 import utils.PytorchHelp as util
 
@@ -172,14 +172,6 @@ class PytorchMulticlass(DNNTask, HTCondorWorkflow, law.LocalWorkflow):
             num_workers=8,  # , shuffle=True  # self.batch_size
         )
 
-        """
-        at some point, we have to include k fold cross validation
-        https://github.com/christianversloot/machine-learning-articles/blob/main/how-to-use-k-fold-cross-validation-with-pytorch.md
-        from sklearn.model_selection import KFold
-        kfold = KFold(n_splits=k_folds, shuffle=True)
-        for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
-        """
-
         # declare lighnting callbacks
         early_stop_callback = pl.callbacks.early_stopping.EarlyStopping(
             monitor="val_acc",
@@ -291,3 +283,182 @@ class PytorchMulticlass(DNNTask, HTCondorWorkflow, law.LocalWorkflow):
         torch.save(model, self.output()["model"].path)
         self.output()["loss_stats"].dump(model.loss_stats)
         self.output()["accuracy_stats"].dump(model.accuracy_stats)
+
+
+class PytorchCrossVal(DNNTask, HTCondorWorkflow, law.LocalWorkflow):
+    # define it here again so training can be started from here
+    kfold = IntParameter(default=5)
+
+    def create_branch_map(self):
+        # overwrite branch map
+        n = 1
+        return list(range(n))
+
+        # return {i: i for i in range(n)}
+
+    def requires(self):
+        # return PrepareDNN.req(self)
+        return {
+            "data": CrossValidationPrep.req(self, kfold=self.kfold),
+            "mean_std": ArrayNormalisation.req(self),
+        }
+
+    def output(self):
+        return self.local_target("performance.json")
+
+    def store_parts(self):
+        # debug_str = ''
+        if self.debug:
+            debug_str = "debug"
+        else:
+            debug_str = ""
+
+        # put hyperparameters in path to make an easy optimization search
+        return (
+            super(PytorchCrossVal, self).store_parts()
+            + (self.channel,)
+            # + (self.n_layers,)
+            + (self.n_nodes,)
+            + (self.dropout,)
+            + (self.batch_size,)
+            + (self.learning_rate,)
+            + (debug_str,)
+        )
+
+    def calc_class_weights(self, y_train, norm=1, sqrt=False):
+        # calc class weights to battle imbalance
+        # norm to tune down huge factors, sqrt to smooth the distribution
+        from sklearn.utils import class_weight
+
+        weight_array = norm * class_weight.compute_class_weight(
+            "balanced",
+            classes=np.unique(np.argmax(y_train, axis=-1)),
+            y=np.argmax(y_train, axis=-1),
+        )
+
+        if sqrt:
+            # smooth by exponential function
+            # return dict(enumerate(np.sqrt(weight_array)))
+            return dict(enumerate((weight_array) ** 0.88))
+        if not sqrt:
+            # set at minimum to 1.0
+            # return dict(enumerate([a if a>1.0 else 1.0 for a in weight_array]))
+            return dict(enumerate(weight_array))
+
+    def reset_weights(self, m):
+        """
+        Try resetting model weights to avoid
+        weight leakage.
+        """
+        for layer in m.children():
+            if hasattr(layer, "reset_parameters"):
+                print(f"Reset trainable parameters of layer = {layer}")
+                layer.reset_parameters()
+
+    def run(self):
+
+        # define dimensions, working with aux template for processes
+        n_variables = len(self.config_inst.variables)
+        n_processes = len(self.config_inst.get_aux("DNN_process_template").keys())
+
+        # definition for the normalization layer
+        means, stds = (
+            self.input()["mean_std"]["means_stds"].load()[0],
+            self.input()["mean_std"]["means_stds"].load()[1],
+        )
+
+        performance = {}
+
+        for i in range(self.kfold):
+
+            # only load needed data set config in each step
+            X_train = self.input()["data"]["cross_val_{}".format(i)][
+                "cross_val_X_train_{}".format(i)
+            ].load()
+            y_train = self.input()["data"]["cross_val_{}".format(i)][
+                "cross_val_y_train_{}".format(i)
+            ].load()
+            X_val = self.input()["data"]["cross_val_{}".format(i)][
+                "cross_val_X_val_{}".format(i)
+            ].load()
+            y_val = self.input()["data"]["cross_val_{}".format(i)][
+                "cross_val_y_val_{}".format(i)
+            ].load()
+
+            class_weights = self.calc_class_weights(y_train)
+
+            # declare model
+            model = util.MulticlassClassification(
+                num_feature=n_variables,
+                num_class=n_processes,
+                means=means,
+                stds=stds,
+                dropout=self.dropout,
+                class_weights=torch.tensor(list(class_weights.values())),
+                n_nodes=self.n_nodes,
+            )
+
+            # weight resetting
+            model.apply(self.reset_weights)
+
+            # datasets are loaded
+            train_dataset = util.ClassifierDataset(
+                torch.from_numpy(X_train).float(), torch.from_numpy(y_train).float()
+            )
+            val_dataset = util.ClassifierDataset(
+                torch.from_numpy(X_val).float(), torch.from_numpy(y_val).float()
+            )
+
+            # define data
+            data_collection = util.DataModuleClass(
+                X_train,
+                y_train,
+                X_val,
+                y_val,
+                # X_test,
+                # y_test,
+                self.batch_size,
+                n_processes,
+                self.steps_per_epoch,
+            )
+
+            # Trainer, for gpu gpus=1
+            trainer = pl.Trainer(
+                max_epochs=self.epochs,
+                num_nodes=1,
+                enable_progress_bar=False,
+                check_val_every_n_epoch=1,
+            )
+
+            trainer.fit(model, data_collection)
+
+            # Print fold results
+            print("K-FOLD CROSS VALIDATION RESULTS FOR {} FOLDS".format(i))
+            print("--------------------------------")
+
+            # for key, value in results.items():
+            print(
+                "Latest accuracy train: {} val: {}".format(
+                    model.accuracy_stats["train"][-1], model.accuracy_stats["val"][-1]
+                )
+            )
+            print(
+                "Latest loss train: {} val: {} \n".format(
+                    model.loss_stats["train"][-1], model.loss_stats["val"][-1]
+                )
+            )
+            # sum += value
+            # print(f'Average: {sum/len(results.items())} %')
+
+            performance.update(
+                {
+                    i: [
+                        model.accuracy_stats["train"][-1],
+                        model.accuracy_stats["val"][-1],
+                        model.loss_stats["train"][-1],
+                        model.loss_stats["val"][-1],
+                    ]
+                }
+            )
+
+        self.output().dump(performance)
